1183dac3ecce7db8caedf19990354e287f032e2e
HoNile <nicolas_cohen@hotmail.fr>
2019-07-12 19:01:27 +0200

    create lib vec3, add reflection and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+use vec3::Vec3f32;
+
 #[derive(Clone)]
 struct Rgb {
     r: f32,
@@ -8,28 +10,17 @@ struct Rgb {
     b: f32,
 }
 
-struct Vec3f32 {
+#[derive(Clone)]
+struct Vec2f32 {
     x: f32,
     y: f32,
-    z: f32,
-}
-
-impl Vec3f32 {
-    fn normalize(self: &mut Self) {
-        let length = ((self.x * self.x) + (self.y * self.y) + (self.z * self.z)).sqrt();
-        self.x = self.x / length;
-        self.y = self.y / length;
-        self.z = self.z / length;
-    }
-}
-
-fn scalar_product(v1: &Vec3f32, v2: &Vec3f32) -> f32 {
-    v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
 }
 
 #[derive(Clone)]
 struct Material {
+    albedo: Vec2f32,
     diffuse_color: Rgb,
+    specular_exponent: f32,
 }
 
 struct Sphere {
@@ -43,12 +34,25 @@ struct Light {
     intensity: f32,
 }
 
+fn scalar_product(v1: &Vec3f32, v2: &Vec3f32) -> f32 {
+    v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+}
+
+fn reflect(i: &Vec3f32, n: &Vec3f32) -> Vec3f32 {
+    // TODO remove or test this comment: i + n
+    Vec3f32::new(
+        i.x - n.x * 2. * scalar_product(i, n),
+        i.y - n.y * 2. * scalar_product(i, n),
+        i.z - n.z * 2. * scalar_product(i, n),
+    )
+}
+
 fn scene_intersect(
     orig: &Vec3f32,
     dir: &Vec3f32,
-    spheres: &Vec<Sphere>,
+    spheres: &[Sphere],
     hit: &mut Vec3f32,
-    N: &mut Vec3f32,
+    n: &mut Vec3f32,
     material: &mut Material,
 ) -> bool {
     let mut sphere_dist = std::f32::MAX;
@@ -57,32 +61,24 @@ fn scene_intersect(
         let ret_ray_intersect = s.ray_intersect(orig, dir, &mut dist_i);
         if ret_ray_intersect && dist_i < sphere_dist {
             sphere_dist = dist_i;
-            *hit = Vec3f32 {
-                x: orig.x + dir.x * dist_i,
-                y: orig.y + dir.y * dist_i,
-                z: orig.z + dir.z * dist_i,
-            };
-            *N = Vec3f32 {
-                x: hit.x - s.center.x,
-                y: hit.y - s.center.y,
-                z: hit.z - s.center.z,
-            };
-            N.normalize();
+            *hit = orig + dir * dist_i;
+            *n = hit.clone() - &s.center;
+            n.normalize();
             *material = s.material.clone();
         }
     }
-    return sphere_dist < 1000.0;
+    sphere_dist < 1000.0
 }
 
 impl Sphere {
     fn ray_intersect(self: &Sphere, orig: &Vec3f32, dir: &Vec3f32, t0: &mut f32) -> bool {
-        let L = Vec3f32 {
-            x: self.center.x - orig.x,
-            y: self.center.y - orig.y,
-            z: self.center.z - orig.z,
-        };
-        let tca = scalar_product(&L, dir);
-        let d2 = scalar_product(&L, &L) - tca * tca;
+        let l = Vec3f32::new(
+            self.center.x - orig.x,
+            self.center.y - orig.y,
+            self.center.z - orig.z,
+        );
+        let tca = scalar_product(&l, dir);
+        let d2 = scalar_product(&l, &l) - tca * tca;
         if d2 > self.radius * self.radius {
             return false;
         }
@@ -95,30 +91,24 @@ impl Sphere {
         if *t0 < 0.0 {
             return false;
         }
-        return true;
+        true
     }
 }
 
-fn cast_ray(orig: &Vec3f32, dir: &Vec3f32, spheres: &Vec<Sphere>, lights: &Vec<Light>) -> Rgb {
-    let mut point = Vec3f32 {
-        x: 0.,
-        y: 0.,
-        z: 0.,
-    };
-    let mut N = Vec3f32 {
-        x: 0.,
-        y: 0.,
-        z: 0.,
-    };
+fn cast_ray(orig: &Vec3f32, dir: &Vec3f32, spheres: &[Sphere], lights: &[Light]) -> Rgb {
+    let mut point = Vec3f32::new(0., 0., 0.);
+    let mut n = Vec3f32::new(0., 0., 0.);
     let mut material = Material {
+        albedo: Vec2f32 { x: 0., y: 0. },
         diffuse_color: Rgb {
             r: 0.,
             g: 0.,
             b: 0.,
         },
+        specular_exponent: 0.,
     };
 
-    if !scene_intersect(orig, dir, spheres, &mut point, &mut N, &mut material) {
+    if !scene_intersect(orig, dir, spheres, &mut point, &mut n, &mut material) {
         return Rgb {
             r: 0.2,
             g: 0.7,
@@ -127,23 +117,30 @@ fn cast_ray(orig: &Vec3f32, dir: &Vec3f32, spheres: &Vec<Sphere>, lights: &Vec<L
     }
 
     let mut diffuse_light_intensity = 0.;
+    let mut specular_light_intensity = 0.;
     for l in lights {
-        let mut light_dir = Vec3f32 {
-            x: l.position.x - point.x,
-            y: l.position.y - point.y,
-            z: l.position.z - point.z,
-        };
+        let mut light_dir = &l.position - &point;
+        let _light_distance = light_dir.norm(); // TODO in progress
         light_dir.normalize();
-        diffuse_light_intensity += l.intensity * scalar_product(&light_dir, &N).max(0.);
+        diffuse_light_intensity += l.intensity * scalar_product(&light_dir, &n).max(0.);
+        let mut vec_reflect = reflect(&(light_dir * -1.), &n);
+        vec_reflect = vec_reflect * -1.;
+        specular_light_intensity += scalar_product(&vec_reflect, &dir)
+            .max(0.)
+            .powf(material.specular_exponent)
+            * l.intensity
     }
-    return Rgb {
-        r: material.diffuse_color.r * diffuse_light_intensity,
-        g: material.diffuse_color.g * diffuse_light_intensity,
-        b: material.diffuse_color.b * diffuse_light_intensity,
-    };
+    Rgb {
+        r: material.diffuse_color.r * diffuse_light_intensity * material.albedo.x
+            + specular_light_intensity * material.albedo.y,
+        g: material.diffuse_color.g * diffuse_light_intensity * material.albedo.x
+            + specular_light_intensity * material.albedo.y,
+        b: material.diffuse_color.b * diffuse_light_intensity * material.albedo.x
+            + specular_light_intensity * material.albedo.y,
+    }
 }
 
-fn render(spheres: &Vec<Sphere>, lights: &Vec<Light>) -> std::io::Result<()> {
+fn render(spheres: &[Sphere], lights: &[Light]) -> std::io::Result<()> {
     const WIDTH: i32 = 1024;
     const HEIGHT: i32 = 728;
     const FOV: f32 = (std::f64::consts::PI / 2.0) as f32;
@@ -163,22 +160,14 @@ fn render(spheres: &Vec<Sphere>, lights: &Vec<Light>) -> std::io::Result<()> {
                 (2.0 * (i as f32 + 0.5) / WIDTH as f32 - 1.0) * (FOV / 2.).tan() * WIDTH as f32
                     / HEIGHT as f32;
             let yf = -(2.0 * (j as f32 + 0.5) / HEIGHT as f32 - 1.0) * (FOV / 2.).tan();
-            let mut dir = Vec3f32 {
-                x: xf,
-                y: yf,
-                z: -1.0,
-            };
-            dir.normalize();
-            framebuffer[(i + j * WIDTH) as usize] = cast_ray(
-                &Vec3f32 {
-                    x: 0.0,
-                    y: 0.0,
-                    z: 0.0,
-                },
-                &dir,
-                spheres,
-                lights,
+            let mut dir = Vec3f32::new(
+                xf,
+                yf,
+                -1.0,
             );
+            dir.normalize();
+            framebuffer[(i + j * WIDTH) as usize] =
+                cast_ray(&Vec3f32::new(0.0, 0.0, 0.0), &dir, spheres, lights);
         }
     }
 
@@ -199,66 +188,60 @@ fn render(spheres: &Vec<Sphere>, lights: &Vec<Light>) -> std::io::Result<()> {
 
 fn main() -> std::io::Result<()> {
     let ivory = Material {
+        albedo: Vec2f32 { x: 0.6, y: 0.3 },
         diffuse_color: Rgb {
             r: 0.4,
             g: 0.4,
             b: 0.3,
         },
+        specular_exponent: 50.,
     };
     let red_rubber = Material {
+        albedo: Vec2f32 { x: 0.9, y: 0.1 },
         diffuse_color: Rgb {
             r: 0.3,
             g: 0.1,
             b: 0.1,
         },
+        specular_exponent: 50.,
     };
 
     let spheres = vec![
         Sphere {
-            center: Vec3f32 {
-                x: -3.0,
-                y: 0.0,
-                z: -16.0,
-            },
+            center: Vec3f32::new(-3.0, 0.0, -16.0),
             radius: 2.0,
             material: ivory.clone(),
         },
         Sphere {
-            center: Vec3f32 {
-                x: -1.0,
-                y: -1.5,
-                z: -12.0,
-            },
+            center: Vec3f32::new(-1.0, -1.5, -12.0),
             radius: 2.0,
             material: red_rubber.clone(),
         },
         Sphere {
-            center: Vec3f32 {
-                x: 1.5,
-                y: -0.5,
-                z: -18.0,
-            },
-            radius: 2.0,
+            center: Vec3f32::new(1.5, -0.5, -18.0),
+            radius: 3.0,
             material: red_rubber.clone(),
         },
         Sphere {
-            center: Vec3f32 {
-                x: 7.0,
-                y: 5.0,
-                z: -18.0,
-            },
-            radius: 2.0,
+            center: Vec3f32::new(7.0, 5.0, -18.0),
+            radius: 4.0,
             material: ivory.clone(),
         },
     ];
 
-    let lights = vec![Light {
-        position: Vec3f32 {
-            x: -20.,
-            y: 20.,
-            z: 20.,
+    let lights = vec![
+        Light {
+            position: Vec3f32::new(-20., 20., 20.),
+            intensity: 1.5,
         },
-        intensity: 1.5,
-    }];
+        Light {
+            position: Vec3f32::new(30., 50., -25.),
+            intensity: 1.8,
+        },
+        Light {
+            position: Vec3f32::new(30., 20., 30.),
+            intensity: 1.7,
+        },
+    ];
     render(&spheres, &lights)
 }
---
+++ src/vec3.rs
@@ -0,0 +1,228 @@
+use std::ops::{Add, Div, Mul, Sub};
+
+//TODO this should be generic
+
+#[derive(Clone)]
+pub struct Vec3f32 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+// https://github.com/rust-lang/rust/issues/21188
+impl Add<Vec3f32> for Vec3f32 {
+    type Output = Vec3f32; // https://stackoverflow.com/questions/39115363/why-do-rusts-operators-have-the-type-output-variable
+    fn add(self, other: Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x + other.x, self.y + other.y, self.z + other.z)
+    }
+}
+impl<'a> Add<Vec3f32> for &'a Vec3f32 {
+    type Output = Vec3f32; // https://stackoverflow.com/questions/39115363/why-do-rusts-operators-have-the-type-output-variable
+    fn add(self, other: Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x + other.x, self.y + other.y, self.z + other.z)
+    }
+}
+impl<'a> Add<&'a Vec3f32> for Vec3f32 {
+    type Output = Vec3f32; // https://stackoverflow.com/questions/39115363/why-do-rusts-operators-have-the-type-output-variable
+    fn add(self, other: &Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x + other.x, self.y + other.y, self.z + other.z)
+    }
+}
+impl<'a> Add<&'a Vec3f32> for &'a Vec3f32 {
+    type Output = Vec3f32; // https://stackoverflow.com/questions/39115363/why-do-rusts-operators-have-the-type-output-variable
+    fn add(self, other: &Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x + other.x, self.y + other.y, self.z + other.z)
+    }
+}
+
+impl Add<f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn add(self, coef: f32) -> Vec3f32 {
+        Vec3f32::new(self.x + coef, self.y + coef, self.z + coef)
+    }
+}
+impl<'a> Add<f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn add(self, coef: f32) -> Vec3f32 {
+        Vec3f32::new(self.x + coef, self.y + coef, self.z + coef)
+    }
+}
+impl<'a> Add<&'a f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn add(self, coef: &f32) -> Vec3f32 {
+        Vec3f32::new(self.x + *coef, self.y + *coef, self.z + *coef)
+    }
+}
+impl<'a> Add<&'a f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn add(self, coef: &f32) -> Vec3f32 {
+        Vec3f32::new(self.x + *coef, self.y + *coef, self.z + *coef)
+    }
+}
+
+impl Sub<Vec3f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn sub(self, other: Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x - other.x, self.y - other.y, self.z - other.z)
+    }
+}
+impl<'a> Sub<Vec3f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn sub(self, other: Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x - other.x, self.y - other.y, self.z - other.z)
+    }
+}
+impl<'a> Sub<&'a Vec3f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn sub(self, other: &Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x - other.x, self.y - other.y, self.z - other.z)
+    }
+}
+impl<'a> Sub<&'a Vec3f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn sub(self, other: &Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x - other.x, self.y - other.y, self.z - other.z)
+    }
+}
+
+impl Sub<f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn sub(self, coef: f32) -> Vec3f32 {
+        Vec3f32::new(self.x - coef, self.y - coef, self.z - coef)
+    }
+}
+impl<'a> Sub<f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn sub(self, coef: f32) -> Vec3f32 {
+        Vec3f32::new(self.x - coef, self.y - coef, self.z - coef)
+    }
+}
+impl<'a> Sub<&'a f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn sub(self, coef: &f32) -> Vec3f32 {
+        Vec3f32::new(self.x - *coef, self.y - *coef, self.z - *coef)
+    }
+}
+impl<'a> Sub<&'a f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn sub(self, coef: &f32) -> Vec3f32 {
+        Vec3f32::new(self.x - *coef, self.y - *coef, self.z - *coef)
+    }
+}
+
+impl Mul<Vec3f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn mul(self, other: Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x * other.x, self.y * other.y, self.z * other.z)
+    }
+}
+impl<'a> Mul<Vec3f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn mul(self, other: Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x * other.x, self.y * other.y, self.z * other.z)
+    }
+}
+impl<'a> Mul<&'a Vec3f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn mul(self, other: &Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x * other.x, self.y * other.y, self.z * other.z)
+    }
+}
+impl<'a> Mul<&'a Vec3f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn mul(self, other: &Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x * other.x, self.y * other.y, self.z * other.z)
+    }
+}
+
+impl Mul<f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn mul(self, coef: f32) -> Vec3f32 {
+        Vec3f32::new(self.x * coef, self.y * coef, self.z * coef)
+    }
+}
+impl<'a> Mul<f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn mul(self, coef: f32) -> Vec3f32 {
+        Vec3f32::new(self.x * coef, self.y * coef, self.z * coef)
+    }
+}
+impl<'a> Mul<&'a f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn mul(self, coef: &f32) -> Vec3f32 {
+        Vec3f32::new(self.x * *coef, self.y * *coef, self.z * *coef)
+    }
+}
+impl<'a> Mul<&'a f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn mul(self, coef: &f32) -> Vec3f32 {
+        Vec3f32::new(self.x * *coef, self.y * *coef, self.z * *coef)
+    }
+}
+
+impl Div<Vec3f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn div(self, other: Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x / other.x, self.y / other.y, self.z / other.z)
+    }
+}
+impl<'a> Div<Vec3f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn div(self, other: Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x / other.x, self.y / other.y, self.z / other.z)
+    }
+}
+impl<'a> Div<&'a Vec3f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn div(self, other: &Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x / other.x, self.y / other.y, self.z / other.z)
+    }
+}
+impl<'a> Div<&'a Vec3f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn div(self, other: &Vec3f32) -> Vec3f32 {
+        Vec3f32::new(self.x / other.x, self.y / other.y, self.z / other.z)
+    }
+}
+
+impl Div<f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn div(self, coef: f32) -> Vec3f32 {
+        Vec3f32::new(self.x / coef, self.y / coef, self.z / coef)
+    }
+}
+impl<'a> Div<f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn div(self, coef: f32) -> Vec3f32 {
+        Vec3f32::new(self.x / coef, self.y / coef, self.z / coef)
+    }
+}
+impl<'a> Div<&'a f32> for Vec3f32 {
+    type Output = Vec3f32;
+    fn div(self, coef: &f32) -> Vec3f32 {
+        Vec3f32::new(self.x / *coef, self.y / *coef, self.z / *coef)
+    }
+}
+impl<'a> Div<&'a f32> for &'a Vec3f32 {
+    type Output = Vec3f32;
+    fn div(self, coef: &f32) -> Vec3f32 {
+        Vec3f32::new(self.x / *coef, self.y / *coef, self.z / *coef)
+    }
+}
+
+impl Vec3f32 {
+    pub fn new(x: f32, y: f32, z: f32) -> Vec3f32 {
+        Vec3f32 { x, y, z }
+    }
+
+    pub fn norm(self: &Self) -> f32 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    pub fn normalize(self: &mut Self) {
+        let length = ((self.x * self.x) + (self.y * self.y) + (self.z * self.z)).sqrt();
+        self.x /= length;
+        self.y /= length;
+        self.z /= length;
+    }
+}
